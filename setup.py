5b48330cb1bf9643c7ea3667b3db6199e4c30ed3
timlac <tim@lachmann.nu>
2023-09-28 17:11:43 +0200

    added MANIFEST file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nexa-definitions-and-mappings',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     data_files=[("json_files", ["py_sentimotion_definitions/sentimotion_definitions.json"])],
     install_requires=[

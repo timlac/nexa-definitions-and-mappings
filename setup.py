6b62fb2c516250d51d44c2d710db21341a2e4647
timlac <tim@lachmann.nu>
2023-09-28 17:07:40 +0200

    trying data_files in setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='nexa-definitions-and-mappings',
-    version='0.9',
+    version='1.0',
     packages=find_packages(),
+    data_files=[("json_files", ["py_sentimotion_definitions/sentimotion_definitions.json"])],
     install_requires=[
     ],
 )

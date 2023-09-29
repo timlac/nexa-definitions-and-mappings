394a5caa84fa87a6c0cbdeb14aac9c054ca4758e
timlac <tim@lachmann.nu>
2023-09-29 11:07:36 +0200

    changed setup to new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nexa-definitions-and-mappings',
-    version='1.3',
+    version='1.4',
     packages=find_packages(),
     include_package_data=True,
     # data_files=[("json_files", ["py_sentimotion_definitions/sentimotion_definitions.json"])],

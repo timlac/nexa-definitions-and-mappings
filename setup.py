db6a3a9a1008092edb0b33a9fa7dd14a15635a40
timlac <tim@lachmann.nu>
2023-09-29 11:08:43 +0200

    changed setup to invlude data files

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='nexa-definitions-and-mappings',
-    version='1.4',
+    version='1.5',
     packages=find_packages(),
     include_package_data=True,
-    # data_files=[("json_files", ["py_sentimotion_definitions/sentimotion_definitions.json"])],
+    data_files=[("json_files", ["definitions/sentimotion_definitions.json"])],
     install_requires=[
     ],
 )

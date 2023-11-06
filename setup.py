2c4023dbfbed392f0b5c16db5eb731cc8a6513f7
timlac <tim@lachmann.nu>
2023-11-06 16:47:38 +0100

    changed name in manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nexa-definitions-and-mappings',
-    version='1.9',
+    version='1.91',
     packages=find_packages(),
     include_package_data=True,
     # data_files=[("json_files", ["definitions/sentimotion_definitions.json"])],
